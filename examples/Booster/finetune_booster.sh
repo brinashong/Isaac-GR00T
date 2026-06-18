e0c9aed3f6facfa1fb91fc5cc40d4fc2bf09cf4c
Brina Shong <brinashong@gmail.com>
2026-06-18 19:58:18 +0800

    update to latest trained script

--- examples/Booster/finetune_booster.sh
+++ examples/Booster/finetune_booster.sh
@@ -1,12 +1,12 @@
 set -x -e
 
-export DATASET_PATH=examples/Booster/wave # meet-and-greet
+export DATASET_PATH=examples/Booster/meet-and-greet_filter-jvel-jtau_50Hz_4Jun # meet-and-greet
 export OUTPUT_DIR=/mnt/ssd-server/eai_dataset/groot_models/wave-n1d7
-export EXPERIMENT_NAME=exp58-wave-absolute-action-tune-loss-20May
+export EXPERIMENT_NAME=exp10-meet-and-greet-absolute-action-conditioning-tune-loss-50Hz-17Jun
 export WANDB_ENTITY="groot-sde"
 
 export NUM_GPUS=1
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 # CUDA_VISIBLE_DEVICES=2 python3 \
 #--base_model_path /mnt/ssd-server/eai_dataset/groot_models/meet-and-greet/exp36-meet-and-greet/checkpoint-20000 \
@@ -21,7 +21,7 @@ python3 \
     --output_dir $OUTPUT_DIR \
     --save_steps 2000 \
     --save_total_limit 20 \
-    --max_steps 40000 \
+    --max_steps 50000 \
     --warmup_ratio 0.05 \
     --weight_decay 1e-5 \
     --learning_rate 1e-4 \
@@ -29,8 +29,10 @@ python3 \
     --lambda_accel 0.0 \
     --lambda_continuity 5e-3 \
     --use_stats_norm_scale \
+    --use_prev_action_conditioning \
     --use_wandb \
     --episode_sampling_rate 1.0 \
+    --state_dropout_prob 0.2 \
     --num_shards_per_epoch 20 \
     --shard_size 512 \
     --entity_name $WANDB_ENTITY \
@@ -39,4 +41,4 @@ python3 \
     --color_jitter_params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
     --dataloader_num_workers 4
 
-    # --use_prev_action_conditioning
+    # --use_prev_action_conditioning \
--- examples/Booster/finetune_booster2.sh
+++ examples/Booster/finetune_booster2.sh
@@ -1,8 +1,8 @@
 set -x -e
 
-export DATASET_PATH=examples/Booster/wave
-export OUTPUT_DIR=/root/sde_ws/src/gr00t_inference/models/wave
-export EXPERIMENT_NAME=exp5
+export DATASET_PATH=examples/Booster/meet-and-greet_filter-jvel-jtau_50Hz_4Jun
+export OUTPUT_DIR=/mnt/ssd-server/eai_dataset/groot_models/wave-n1d7
+export EXPERIMENT_NAME=exp9-meet-and-greet-absolute-action-tune-loss-50Hz-8Jun
 export WANDB_ENTITY="groot-sde"
 
 export NUM_GPUS=1
@@ -19,23 +19,31 @@ export CUDA_VISIBLE_DEVICES=2
 # --modality_config_path examples/Booster/booster_config.py \
 python3 \
     gr00t/experiment/launch_finetune.py \
-    --base_model_path nvidia/GR00T-N1.6-3B \
     --dataset_path $DATASET_PATH  \
+    --base_model_path nvidia/GR00T-N1.7-3B \
     --embodiment_tag NEW_EMBODIMENT \
+    --modality_config_path examples/Booster/booster_config.py \
     --num_gpus $NUM_GPUS \
     --output_dir $OUTPUT_DIR \
-    --save_steps 1000 \
-    --save_total_limit 10 \
-    --max_steps 20000 \
+    --save_steps 2000 \
+    --save_total_limit 20 \
+    --max_steps 50000 \
     --warmup_ratio 0.05 \
     --weight_decay 1e-5 \
     --learning_rate 1e-4 \
+    --lambda_smooth 5e-3 \
+    --lambda_accel 0.0 \
+    --lambda_continuity 5e-3 \
+    --use_stats_norm_scale \
     --use_wandb \
-    --episode_sampling_rate 0.5 \
+    --episode_sampling_rate 1.0 \
+    --state_dropout_prob 0.2 \
     --num_shards_per_epoch 20 \
     --shard_size 512 \
     --entity_name $WANDB_ENTITY \
     --experiment_name $EXPERIMENT_NAME \
-    --global_batch_size 64 \
+    --global_batch_size 128 \
     --color_jitter_params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
     --dataloader_num_workers 4
+
+    # --use_prev_action_conditioning \
